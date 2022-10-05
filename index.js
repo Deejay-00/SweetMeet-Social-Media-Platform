1265a316b84799a0e71a22d4e7b0c4d9487aa7c8
Jamiu <jammy190@gmail.com>
2022-10-05 15:29:25 +0100

    Final modifications to complete project

--- index.js
+++ index.js
@@ -9,8 +9,16 @@ const messageSearch = document.querySelector("#message-search")
 //THEME
 const theme = document.querySelector("#theme")
 const themeModal = document.querySelector(".customize-theme")
+const fontSizes = document.querySelector(".choose-size span")
+var root = document.querySelector(":root")
+const colorPalette = document.querySelectorAll(".choose-color span")
 
-// ====================== SIDEBAR ======================
+//Notification
+let closeNotification = document.querySelector("#notifications")
+
+
+        // ====================== SIDEBAR ======================
+   
 // Remove active class from all menu items
 const changeActiveItem = () => {
     menuItems.forEach(item => {
@@ -32,13 +40,21 @@ menuItems.forEach(item => {
 
     })
 })
+// const popupClose = (e) => {
+//     if(e.target.classList.contains("notification-popup")){
+//         closeNotification.style.display = "none"
+//     } 
+// }
 
-// menuItems.addEventListener("click", (e) => {
+// closeNotification.addEventListener("click", popupClose)
+
+// closeNotification.addEventListener("click", (e) => {
 //     const isClosest = e.target.closest("notification-popup")
 //         if (!isClosest && item.classList.contains("active")){
 //             item.classList.remove("active")
 //         }
 // })
+
 
 /* ====================== MESSAGES ======================*/
 // search chat function
@@ -85,3 +101,64 @@ messageSearch.addEventListener("keyup", searchMessage)
 
  // close modal
 themeModal.addEventListener("click", closeThemeModal)
+
+
+// ================= FONT SIZE =================
+const removeSizeSelector = () => {
+    fontSizes.forEach(size => {
+        size.classList.remove("active")
+    })
+}
+fontSizes.forEach(size => {
+    size.addEventListener("click", () => {
+        removeSizeSelector();
+        let fontSize
+        size.classList.toggle("active")
+
+        if(size.classList.contains("font-size-1")){
+            fontSize = "10px"
+            root.style.setProperty("----sticky-top-left", "5.4rem")
+            root.style.setProperty("----sticky-top-right", "5.4rem")
+        } else if (size.classList.contains("font-size-2")){
+            fontSize = "13px"
+            root.style.setProperty("----sticky-top-left", "5.4rem")
+            root.style.setProperty("----sticky-top-right", "-7rem")
+        } else if (size.classList.contains("font-size-3")){
+            fontSize = "16px"
+            root.style.setProperty("----sticky-top-left", "-2rem")
+            root.style.setProperty("----sticky-top-right", "-17rem")
+        } else if (size.classList.contains("font-size-4")){
+            fontSize = "19px"
+            root.style.setProperty("----sticky-top-left", "-5rem")
+            root.style.setProperty("----sticky-top-right", "-25rem")
+        } else if (size.classList.contains("font-size-5")){
+            fontSize = "22px"
+            root.style.setProperty("----sticky-top-left", "-12rem")
+            root.style.setProperty("----sticky-top-right", "-35rem")
+        }
+        //change font size of the root html element
+    document.querySelector("html").style.fontSize = fontSize
+    })
+    
+})
+
+// change primary colors 
+colorPalette.forEach(color => {
+    color.addEventListener("click", () => {
+        let primary
+        
+        if(color.classList.contains("color-1")){
+            primaryHue = 252
+        } else if(color.classList.contains("color-2")){
+            primaryHue = 52
+        } else if(color.classList.contains("color-3")){
+            primaryHue = 352
+        } else if(color.classList.contains("color-4")){
+            primaryHue = 152
+        } else if(color.classList.contains("color-5")){
+            primaryHue = 202
+        }
+        
+        root.style.setProperty("--primary-color-hue: 252", primaryHue)
+    })
+})
